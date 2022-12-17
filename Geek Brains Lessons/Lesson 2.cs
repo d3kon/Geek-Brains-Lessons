7babaf1c9efd8983a8d3f62078b1bf463b9d5bc8
Dmitry <d3kon@mail.ru>
2022-12-17 17:18:30 +0800

    + Задача 4

--- Geek Brains Lessons/Lesson 2.cs
+++ Geek Brains Lessons/Lesson 2.cs
@@ -15,6 +15,24 @@
                 Console.WriteLine($"{b} its max");
             }
         }
+        public static void Trio_Max()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if (a > b & a > c)
+            {
+                Console.WriteLine($"{a} its max");
+            }
+            else if (b > a & b > c)
+            {
+                Console.WriteLine($"{b} its max");
+            }
+            else
+            {
+                Console.WriteLine($"{c} its max");
+            }
+        }
 
     }
 }
--- Geek Brains Lessons/Program.cs
+++ Geek Brains Lessons/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Geek_Brains_Lessons;
 
+Console.WriteLine("Задача 2");
 Lesson1.Max();
+Console.WriteLine("Задача 4");
+Lesson1.Trio_Max();
