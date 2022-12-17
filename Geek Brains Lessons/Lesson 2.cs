9de5992b8f2062455a5b04614bd2292432bd8ac6
Dmitry <d3kon@mail.ru>
2022-12-17 17:15:21 +0800

    Добавьте файлы проекта.

---
+++ Geek Brains Lessons/Lesson 2.cs
@@ -0,0 +1,20 @@
+﻿namespace Geek_Brains_Lessons
+{
+    public static class Lesson1
+    {
+        public static void Max()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine($"{a} its max");
+            }
+            else
+            {
+                Console.WriteLine($"{b} its max");
+            }
+        }
+
+    }
+}
---
+++ Geek Brains Lessons/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Geek_Brains_Lessons;
+
+Lesson1.Max();
